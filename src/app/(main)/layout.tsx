2bae523e9eeec3c54f9df99074014fd87a309d78
Ega Destaviano <pastilaku121@gmail.com>
2026-04-13 11:30:36 +0700

    fix: layout header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({
     <div className="flex flex-col min-h-screen w-full">
       <Header />
 
-      <div className="container mx-auto flex-1 py-6">
+      <div className="container mx-auto flex-1 ">
         <ReduxProvider>
           {children}
         </ReduxProvider>
